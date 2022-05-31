0556c5259928a339ffd5ab1d4e8131822722c713
ushioda <yusuke_ushioda@mediado.jp>
2022-05-31 18:55:30 +0900

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  
+  root 'attendances#index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
   patch 'attendances/finish' => 'attendances#finish', as: 'finish'
-  root 'attendances#index'
+  
   resources :attendances
   resources :users
   resources :holidays
