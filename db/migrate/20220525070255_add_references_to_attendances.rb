f78cabf3d6234c6db3a5cc446fdb2df077c0f3e9
ushioda <yusuke_ushioda@mediado.jp>
2022-06-09 10:46:21 +0900

    add

--- db/migrate/20220525070255_add_references_to_attendances.rb
+++ db/migrate/20220525070255_add_references_to_attendances.rb
@@ -1,5 +1,5 @@
 class AddReferencesToAttendances < ActiveRecord::Migration[6.0]
   def change
-    add_reference :attendances, :user,null: false, foreign_key: true
+    add_reference :attendances, :user, null: false, foreign_key: true
   end
 end
